0a45df7476440dd556abb9e635bb2a5a8fd3ae91
Ayesha Iftikhar <seAyeshaiftikhar@gmail.com>
2025-01-17 14:56:52 +0500

    Update story_video.dart

--- lib/widgets/story_video.dart
+++ lib/widgets/story_video.dart
@@ -63,7 +63,7 @@ class StoryVideoState extends State<StoryVideo> {
             VideoPlayerController.file(widget.videoLoader.videoFile!);
 
         playerController!.initialize().then((v) {
-          setState(() {});
+        if (mounted)  setState(() {});
           widget.storyController!.play();
         });
 
@@ -78,7 +78,7 @@ class StoryVideoState extends State<StoryVideo> {
           });
         }
       } else {
-        setState(() {});
+       if (mounted) setState(() {});
       }
     });
   }
